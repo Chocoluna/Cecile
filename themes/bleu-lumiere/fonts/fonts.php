b1371bd5f37a12cf1ab4acd9d01be70270fe6f06
Chocoluna <cl.coutupetra@gmail.com>
2022-07-28 17:11:52 +0200

    font + colors

--- themes/bleu-lumiere/fonts/fonts.php
+++ themes/bleu-lumiere/fonts/fonts.php
@@ -10,12 +10,15 @@ if ( ! function_exists( 'gateway_fonts' ) ) :
         $fonts     = array();
         $subsets   = 'latin,latin-ext';
         /* translators: If there are characters in your language that are not supported by Quattrocento, translate this to 'off'. Do not translate into your own language. */
-        if ( 'off' !== _x( 'on', 'Quattrocento font: on or off', 'gateway' ) ) {
-            $fonts[] = 'Quattrocento:400,700';
+        if ( 'off' !== _x( 'on', 'Frente H1 font: on or off', 'gateway' ) ) {
+            $fonts[] = 'Frente H1:400,700';
         }
         /* translators: If there are characters in your language that are not supported by Fanwood Text, translate this to 'off'. Do not translate into your own language. */
-        if ( 'off' !== _x( 'on', 'Fanwood Text font: on or off', 'gateway' ) ) {
-            $fonts[] = 'Fanwood Text:400,400italic';
+        if ( 'off' !== _x( 'on', 'Raleway Regular font: on or off', 'gateway' ) ) {
+            $fonts[] = 'Raleway Regular:400,400italic';
+        }
+        if ( 'off' !== _x( 'on', 'Raleway Medium font: on or off', 'gateway' ) ) {
+            $fonts[] = 'Raleway Medium:400,400italic';
         }
         /* translators: To add an additional character subset specific to your language, translate this to 'greek', 'cyrillic', 'devanagari' or 'vietnamese'. Do not translate into your own language. */
         $subset = _x( 'no-subset', 'Add new subset (greek, cyrillic, devanagari, vietnamese)', 'gateway' );
